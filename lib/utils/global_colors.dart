b95161c7ae5625e7d3e6348bcffbe68464be4e20
VoidElle <lucadelc@gmail.com>
2025-05-22 18:11:00 +0200

    chore: Aligned design with real app

--- lib/utils/global_colors.dart
+++ lib/utils/global_colors.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 class GlobalColors {
 
-  // Primary color
+  // Primary green color
   static final Color primaryNeonGreenColor = const Color(0xFF08e054);
+
+  // Secondary green color
   static final Color secondaryGreenColor = const Color(0xFFa3deb2);
+
+  // QR code - Body - Black color
+  static final Color qrCodeBodyBlackColor = const Color(0xFF000000);
+
+  // QR code - Angles - Gray color
+  static final Color qrCodeAnglesGrayColor = const Color(0xFF2d2d2d);
 
 }
