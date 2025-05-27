91c41663853e11df0b83dee7a10613c50176174d
VoidElle <lucadelc@gmail.com>
2025-05-27 19:47:38 +0200

    chore: Removed Loading dialog

--- lib/network/network_inteceptor.dart
+++ lib/network/network_inteceptor.dart
@@ -1,35 +1,19 @@
-import 'package:black_theory/widgets/dialogs/loading_dialog.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
-
-import '../main.dart';
 
 class NetworkInterceptor extends InterceptorsWrapper {
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-
-    final BuildContext context = navigatorKey.currentState!.context;
-    LoadingDialog.show(context);
-
     super.onRequest(options, handler);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-
-    final BuildContext context = navigatorKey.currentState!.context;
-    LoadingDialog.dismiss(context);
-
     super.onResponse(response, handler);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
-
-    final BuildContext context = navigatorKey.currentState!.context;
-    LoadingDialog.dismiss(context);
-
     super.onError(err, handler);
   }
 
